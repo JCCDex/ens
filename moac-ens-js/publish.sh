2669ce43a1e203fdf435caa2b68e48f1e69e9a4c
Libre <gin.musheng@gmail.com>
2020-01-10 22:31:17 +0800

    js sdk of moac ens (#1)

---
+++ moac-ens-js/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+username=$(npm whoami)
+echo $username
+if test "$username" = "jccdex"; then
+    if test "$1" = "";then
+        npm version patch --no-git-tag-version
+    else
+        npm version $1 --no-git-tag-version
+    fi
+    if [ -d "lib" ];then
+        rm -r lib
+    fi
+    npm run build
+    npm publish
+else
+    echo "please login with jccdex account"
+    exit 0
+fi
---
+++ moac-ens-js/src/ens-registry.ts
@@ -0,0 +1,188 @@
+import { Moac, smartContract as SmartContract } from "jcc-moac-utils";
+import { ITransactionOption } from "jcc-moac-utils/lib/model/transaction";
+import * as abi from "./abi/ENSRegistry.abi.json";
+
+export default class ENSRegistryContract extends SmartContract {
+  public static readonly rootNode = "0x0000000000000000000000000000000000000000";
+  private _contractAddress: string;
+
+  /**
+   * Creates an instance of ENSRegistryContract.
+   *
+   * Test: `0x5114f0389990f93180c8a1847bac2fa98ca58524`
+   *
+   * Main: `0xea5a36ed305c5033e6ec27edb48009b79290ea51`
+   *
+   * @param {string} contractAddress
+   * @memberof ENSRegistryContract
+   */
+  constructor(contractAddress: string) {
+    super();
+    this._contractAddress = contractAddress;
+  }
+
+  public get contractAddress(): string {
+    return this._contractAddress;
+  }
+
+  public set contractAddress(address: string) {
+    this._contractAddress = address;
+  }
+
+  public initContract(moac: Moac) {
+    super.init(this._contractAddress, moac, abi);
+  }
+
+  /**
+   * Set owner of node
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @param {string} owner address
+   * @param {(err: Error, calldata: string) => void} cb send transaction with calldata by yourself
+   *
+   * You could send transaction via `sendTransaction` directly.
+   *
+   * For DAPP, you could send via wallet sdk API.
+   *
+   * @memberof ENSRegistryContract
+   */
+  public async setOwner(node: string, owner: string, cb: (err: Error, calldata: string) => void) {
+    super
+      .callABI("setOwner", node, owner)
+      .then((calldata) => {
+        cb(null, calldata);
+      })
+      .catch((err) => {
+        cb(err, null);
+      });
+  }
+
+  /**
+   * Set owner of sub node
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @param {string} label you need generate hash from `label` value with [sha3](https://github.com/MOACChain/chain3/blob/master/lib/utils/sha3.js#L29) API in general.
+   * @param {string} owner address
+   * @param {(err: Error, calldata: string) => void} cb send transaction with calldata by yourself
+   *
+   * You could send transaction via `sendTransaction` directly.
+   *
+   * For DAPP, you could send via wallet sdk API.
+   *
+   * @memberof ENSRegistryContract
+   */
+  public async setSubnodeOwner(node: string, label: string, owner: string, cb: (err: Error, calldata: string) => void) {
+    super
+      .callABI("setSubnodeOwner", node, label, owner)
+      .then((calldata) => {
+        cb(null, calldata);
+      })
+      .catch((err) => {
+        cb(err, null);
+      });
+  }
+
+  /**
+   * Set resolver
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @param {string} resolver address
+   * @param {(err: Error, calldata: string) => void} cb send transaction with calldata by yourself
+   *
+   * You could send transaction via `sendTransaction` directly.
+   *
+   * For DAPP, you could send via wallet sdk API.
+   *
+   * @memberof ENSRegistryContract
+   */
+  public async setResolver(node: string, resolver: string, cb: (err: Error, calldata: string) => void) {
+    super
+      .callABI("setResolver", node, resolver)
+      .then((calldata) => {
+        cb(null, calldata);
+      })
+      .catch((err) => {
+        cb(err, null);
+      });
+  }
+
+  /**
+   * Set TTL
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @param {number} ttl
+   * @param {(err: Error, calldata: string) => void} cb send transaction with calldata by yourself
+   *
+   * You could send transaction via `sendTransaction` directly.
+   *
+   * For DAPP, you could send via wallet sdk API.
+   *
+   * @memberof ENSRegistryContract
+   */
+  public async setTTL(node: string, ttl: number, cb: (err: Error, calldata: string) => void) {
+    super
+      .callABI("setTTL", node, ttl)
+      .then((calldata) => {
+        cb(null, calldata);
+      })
+      .catch((err) => {
+        cb(err, null);
+      });
+  }
+
+  /**
+   * Query the owner of node
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @returns {Promise<string>}
+   * @memberof ENSRegistryContract
+   */
+  public async owner(node: string): Promise<string> {
+    const address = await super.callABI("owner", node);
+    return address;
+  }
+
+  /**
+   * Query resolver
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @returns {Promise<string>}
+   * @memberof ENSRegistryContract
+   */
+  public async resolver(node: string): Promise<string> {
+    const address = await super.callABI("resolver", node);
+    return address;
+  }
+
+  /**
+   * Query TTL
+   *
+   * @param {string} node If node is not `rootNode`, you need generate hash from `node` value with [hash](https://github.com/danfinlay/eth-ens-namehash/blob/master/index.js#L29) API in general.
+   * @returns {Promise<number>}
+   * @memberof ENSRegistryContract
+   */
+  public async ttl(node: string): Promise<number> {
+    const ttl = await super.callABI("ttl", node);
+    return ttl;
+  }
+
+  /**
+   * send transaction
+   *
+   * @param {string} amount
+   * @param {string} calldata
+   * @param {string} secret
+   * @param {ITransactionOption} [options]
+   * @returns {Promise<string>}
+   * @memberof ENSRegistryContract
+   */
+  public async sendTransaction(
+    amount: string,
+    calldata: string,
+    secret: string,
+    options?: ITransactionOption
+  ): Promise<string> {
+    const hash = await this.moac.sendTransactionWithCallData(secret, this._contractAddress, amount, calldata, options);
+    return hash;
+  }
+}
